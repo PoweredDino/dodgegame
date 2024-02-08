f814ff61d79e5fe31f82f953804f582dc07aa6a1
PoweredDino <155806954+PoweredDino@users.noreply.github.com>
2024-02-08 11:40:11 +0600

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ window.addEventListener('load', function(){
         constructor(){
             this.keys = [];
             if (isMobile()) {
-                window.addEventListener('mousemove', (e) => {
+                window.addEventListener('mouseclick', (e) => {
                     if (e.x > ArrowDown.x && e.x < ArrowDown.x + (averageSize * 100) && e.y > ArrowDown.y && e.y < ArrowDown.y + (averageSize * 100)){
                         this.keys.push("ArrowDown");
                     }
@@ -79,7 +79,7 @@ window.addEventListener('load', function(){
                         this.keys.push("ArrowUp");
                     }
                 })
-                window.addEventListener('mouseover', (e) => {
+                window.addEventListener('mouseup', (e) => {
                     if (e.x > ArrowDown.x && e.x < ArrowDown.x + (averageSize * 100) && e.y > ArrowDown.y && e.y < ArrowDown.y + (averageSize * 100)){
                         this.keys.splice(this.keys.indexOf("ArrowDown"), 1);
                     }
